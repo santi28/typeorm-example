23a86b8735531ad7d6edd7fe877c375da5330a34
Santiago de Nicolás <santidenicolas@gmail.com>
2024-01-23 02:36:08 -0300

    Add Task entity definition

---
+++ src/entities/tasks.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: "pending", enum: ["pending", "doing", "done"] })
+  status: "pending" | "doing" | "done";
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn({ nullable: true })
+  deletedAt: Date;
+}
