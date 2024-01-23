11ac6184e4c95079092f9648f8721c6a72af38f8
Santiago de Nicolás <santidenicolas@gmail.com>
2024-01-23 02:15:23 -0300

    added user entity columns

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,13 +1,28 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
   name: string;
+  @Column({ unique: true })
   email: string;
 
+  @CreateDateColumn()
   createdAt: Date;
+
+  @UpdateDateColumn()
   updatedAt: Date;
+
+  @DeleteDateColumn({ nullable: true })
   deletedAt: Date;
 }
